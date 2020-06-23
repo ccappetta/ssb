72a638350fd51ecf01729dad8aa426cdf8f415cc
Chris Cappetta <36048795+ccappetta@users.noreply.github.com>
2020-06-23 15:04:28 -0600

    Update ssb_count_usage.sql

--- ssb_count_usage.sql
+++ ssb_count_usage.sql
@@ -1,13 +1,14 @@
 DROP procedure if exists proc_count_usage;
-create procedure proc_count_usage(IN dbname varchar(128), IN item_id varchar(255), IN last_execution timestamp)
+create procedure proc_count_usage(IN dbname varchar(128), IN item_id varchar(255), IN last_execution timestamp, OUT usage_qty integer(11))
 BEGIN
     SET @theSQL=CONCAT('
-
-SELECT COUNT(*) from ',dbname,'.usage_event as usage_event where item_identifier = ''',item_id,''' AND event_timestamp > ''',last_execution,''';
-
-
+    SELECT COUNT(*) INTO @usage_qty from ',dbname,'.usage_event WHERE item_identifier = ''',item_id,''' AND event_timestamp > ''',last_execution,''';
 ');
     PREPARE stmt1 from @theSQL;
     EXECUTE stmt1;
+    SELECT @usage_qty;
+    SET usage_qty = @usage_qty;
     DEALLOCATE PREPARE stmt1;
+
+
 END;
